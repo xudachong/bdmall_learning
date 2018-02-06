8f2dd1c96e9301e9b9ee38b1c189068e50478fcb
xujulong <574408578@qq.com>
2018-02-06 17:11:22 +0800

    commit use the SpringMVC RESTful to improve the URI

---
+++ src/main/java/com/bigdragonmall/task/CloseOrderTask.java
@@ -0,0 +1,18 @@
+package com.bigdragonmall.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: XuJuLong<5 7 4 4 0 8 5 7 8 @ qq.com>
+ * @Description:
+ * @Date: Created in 17:03 2018/2/6
+ * @
+ */
+@Component
+@Slf4j
+public class CloseOrderTask {
+
+
+
+}
